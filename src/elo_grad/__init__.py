f2d3fc9c6328bb755af270a4ff91ae7f64163743
Michael Cooke <cookepm@tcd.ie>
2024-11-08 06:07:48 +0000

    feat: Add PoissonEloEstimator (#38)
    Rating system based on Poisson regression, i.e. the rating system
    is equivalent to Poisson regression with stochastic gradient
    descent. The outcome should be count data though can be used for
    binary outcomes in cases where we are interested in the relative
    risks.
    
    To define this we:
    - introduces PoissonRegression class
    - add ClassifierRatingSystemMixin and RegressionRatingSystemMixin
      which inherit from RatingSystemMixin
    - add a beta parameter to the abstract Model class
    - add an abstract base class BaseEloEstimator
    
    Supporting documentation is added:
    - examples/football.ipynb: a NB applying PoissonEloEstimator
      to Premier League football data
    - docs/feature_ref/poisson.md: mkdocs page outlining the
      theory behind the implementation

--- src/elo_grad/__init__.py
+++ src/elo_grad/__init__.py
@@ -5,17 +5,26 @@ from importlib.metadata import version
 
 from array import array
 from collections import defaultdict
-from typing import Tuple, Optional, Dict, List, Callable, Generator
+from typing import Tuple, Optional, Dict, List, Callable, Generator, Type
 
 import math
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator
-from sklearn.metrics import log_loss
+from sklearn.metrics import log_loss, mean_poisson_deviance
 
 from .plot import HistoryPlotterMixin
 
-__all__ = ["EloEstimator", "LogisticRegression", "RatingSystemMixin", "Regressor", "SGDOptimizer"]
+__all__ = [
+    "ClassifierRatingSystemMixin",
+    "EloEstimator",
+    "LogisticRegression",
+    "PoissonEloEstimator",
+    "PoissonRegression",
+    "RegressionRatingSystemMixin",
+    "Regressor",
+    "SGDOptimizer",
+]
 __version__ = version(__package__)
 
 
@@ -41,13 +50,13 @@ class Regressor:
 
 class Model(abc.ABC):
 
-    loss: str
-
     def __init__(
         self,
+        beta: float,
         default_init_rating: float,
         init_ratings: Optional[Dict[str, Tuple[Optional[int], float]]] = None,
     ) -> None:
+        self.beta: float = beta
         self.ratings: Dict[str, Tuple[Optional[int], float]] = defaultdict(
             lambda: (None, default_init_rating)
         )
@@ -80,9 +89,8 @@ class Optimizer(abc.ABC):
     ) -> Generator[float, None, None]:
         ...
 
-class LogisticRegression(Model):
 
-    loss: str = "log-loss"
+class LogisticRegression(Model):
 
     def __init__(
         self,
@@ -90,12 +98,11 @@ class LogisticRegression(Model):
         default_init_rating: float,
         init_ratings: Optional[Dict[str, Tuple[Optional[int], float]]],
     ) -> None:
-        super().__init__(default_init_rating, init_ratings)
-        self.beta: float = beta
+        super().__init__(beta, default_init_rating, init_ratings)
 
     def calculate_gradient(self, y: int, *args) -> float:
         if y not in {0, 1}:
-            raise ValueError("Invalid result value %s", y)
+            raise ValueError("Invalid score value %s", y)
         y_pred: float = self.calculate_expected_score(*args)
 
         return y - y_pred
@@ -106,6 +113,31 @@ class LogisticRegression(Model):
         # I couldn't see any obvious speed-up from using NumPy/Numba data
         # structures but should revisit this.
         return 1 / (1 + math.pow(10, -sum(args) / (2 * self.beta)))
+
+
+class PoissonRegression(Model):
+
+    def __init__(
+        self,
+        beta: float,
+        default_init_rating: float,
+        init_ratings: Optional[Dict[str, Tuple[Optional[int], float]]],
+    ) -> None:
+        super().__init__(beta, default_init_rating, init_ratings)
+
+    def calculate_gradient(self, y: int, *args) -> float:
+        if not isinstance(y, int):
+            raise ValueError("Invalid score value %s", y)
+        # This is the same as for the logistic regression model
+        # - is this just a property of all exponential family models?
+        y_pred: float = self.calculate_expected_score(*args)
+
+        return y - y_pred
+
+    # We should make maxsize configurable
+    @lru_cache(maxsize=512)
+    def calculate_expected_score(self, *args) -> float:
+        return math.pow(10, sum(args) / (2 * self.beta))
 
 
 class SGDOptimizer(Optimizer):
@@ -134,39 +166,24 @@ class SGDOptimizer(Optimizer):
                 yield self.k_factor[i] * v * entity_grad
 
 
-class RatingSystemMixin:
+class RatingSystemMixin(abc.ABC):
+
+    @abc.abstractmethod
+    def score(self, X, y, sample_weight=None): ...
+
+    def _more_tags(self):
+        return {"requires_y": False}
+
+
+class ClassifierRatingSystemMixin(RatingSystemMixin):
     """
-    Mixin class for rating systems.
+    Mixin class for classification rating systems.
 
     This mixin defines the following functionality:
 
-    - `_estimator_type` class attribute defaulting to `"rating-system"`;
+    - `_estimator_type` class attribute defaulting to `"classifier"`;
     - `score` method that default to :func:`~sklearn.metrics.log_loss`.
     - enforce that `fit` does not require `y` to be passed through the `requires_y` tag.
-
-    Read more in the :ref:`User Guide <rolling_your_own_estimator>`.
-
-    Examples
-    --------
-    >>> import numpy as np
-    >>> from sklearn.base import BaseEstimator
-    >>> from elo_grad import RatingSystemMixin
-    >>> # Mixin classes should always be on the left-hand side for a correct MRO
-    >>> class MyEstimator(RatingSystemMixin, BaseEstimator):
-    ...     def __init__(self, *, param=1):
-    ...         self.param = param
-    ...     def fit(self, X, y=None):
-    ...         self.is_fitted_ = True
-    ...         return self
-    ...     def predict(self, X):
-    ...         return np.full(shape=X.shape[0], fill_value=self.param)
-    >>> estimator = MyEstimator(param=1)
-    >>> X = np.array([[1, 2], [2, 3], [3, 4]])
-    >>> y = np.array([1, 0, 1])
-    >>> estimator.fit(X, y).predict(X)
-    array([1, 1, 1])
-    >>> estimator.score(X, y)
-    0.66...
     """
 
     _estimator_type = "classifier"
@@ -190,22 +207,69 @@ class RatingSystemMixin:
         Returns
         -------
         score : float
-            Mean accuracy of ``self.predict(X)`` w.r.t. `y`.
+            Log-loss of ``self.predict_proba(X)[:, 1]`` w.r.t. `y`.
         """
         return log_loss(
             y, self.predict_proba(X)[:, 1], sample_weight=sample_weight
         )
 
-    def _more_tags(self):
-        return {"requires_y": False}
+    def predict_proba(self, X: pd.DataFrame) -> np.ndarray:
+        preds = self._transform(X, return_expected_score=True)  # type:ignore
+        return np.vstack((1 - preds, preds)).T  # type:ignore
+
+    def predict(self, X: pd.DataFrame) -> np.ndarray:
+        return self.predict_proba(X)[:, 1] > 0.5
 
 
-class EloEstimator(HistoryPlotterMixin, RatingSystemMixin, BaseEstimator):
+class RegressionRatingSystemMixin(RatingSystemMixin):
+    """
+    Mixin class for regression rating systems.
+
+    This mixin defines the following functionality:
+
+    - `_estimator_type` class attribute defaulting to `"regressor"`;
+    - `score` method that default to :func:`~sklearn.metrics.mean_poisson_deviance`.
+    - enforce that `fit` does not require `y` to be passed through the `requires_y` tag.
+    """
+
+    _estimator_type = "regressor"
+
+    def score(self, X, y, sample_weight=None):
+        """
+        Return the mean Poisson deviance on the given test data.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Test samples.
+
+        y : array-like of shape (n_samples,) or (n_samples, n_outputs)
+            True labels for `X`.
+
+        sample_weight : array-like of shape (n_samples,), default=None
+            Sample weights.
+
+        Returns
+        -------
+        score : float
+            Mean Poisson deviance of ``self.predict(X)`` w.r.t. `y`.
+        """
+        return mean_poisson_deviance(
+            y, self.predict(X), sample_weight=sample_weight
+        )
+
+    def predict(self, X: pd.DataFrame) -> np.ndarray:
+        return self._transform(X, return_expected_score=True)  # type:ignore
+
+
+class BaseEloEstimator(HistoryPlotterMixin, BaseEstimator):
     """
     Elo rating system classifier.
 
     Attributes
     ----------
+    model_type : Type[Model]
+        Base model class type.
     beta : float
         Normalization factor for ratings when computing expected score.
     columns : List[str]
@@ -248,18 +312,21 @@ class EloEstimator(HistoryPlotterMixin, RatingSystemMixin, BaseEstimator):
 
     def __init__(
         self,
-        k_factor: float = 20,
-        default_init_rating: float = 1200,
-        beta: float = 200,
-        init_ratings: Optional[Dict[str, Tuple[Optional[int], float]]] = None,
-        entity_cols: Tuple[str, str] = ("entity_1", "entity_2"),
-        score_col: str = "score",
-        additional_regressors: Optional[List[Regressor]] = None,
-        track_rating_history: bool = False,
+        model_type: Type[Model],
+        k_factor: float,
+        default_init_rating: float,
+        beta: float,
+        init_ratings: Optional[Dict[str, Tuple[Optional[int], float]]],
+        entity_cols: Tuple[str, str],
+        score_col: str,
+        additional_regressors: Optional[List[Regressor]],
+        track_rating_history: bool,
     ) -> None:
         """
         Parameters
         ----------
+        model_type : Type[Model]
+            Base model class type.
         k_factor : float
             Elo K-factor/step-size for gradient descent for the entities.
         default_init_rating : float
@@ -284,7 +351,8 @@ class EloEstimator(HistoryPlotterMixin, RatingSystemMixin, BaseEstimator):
         self.beta: float = beta
         self.default_init_rating: float = default_init_rating
         self.init_ratings: Optional[Dict[str, Tuple[Optional[int], float]]] = init_ratings
-        self.model: Model = LogisticRegression(
+        self.model_type: Type[Model] = model_type
+        self.model: Model = model_type(
             beta=beta,
             default_init_rating=default_init_rating,
             init_ratings=init_ratings,
@@ -305,7 +373,7 @@ class EloEstimator(HistoryPlotterMixin, RatingSystemMixin, BaseEstimator):
     def _reinitialize_rating_system(method: Callable):
         """
         Decorator to reinitialize the rating system after parameter changes.
-        Helpful when peforming a grid search.
+        Helpful when performing a grid search.
 
         Parameters
         ----------
@@ -315,7 +383,7 @@ class EloEstimator(HistoryPlotterMixin, RatingSystemMixin, BaseEstimator):
 
         def wrapper(self, **params):
             result = method(self, **params)
-            self.model = LogisticRegression(
+            self.model = self.model_type(
                 beta=self.beta,
                 default_init_rating=self.default_init_rating,
                 init_ratings=self.init_ratings,
@@ -412,9 +480,182 @@ class EloEstimator(HistoryPlotterMixin, RatingSystemMixin, BaseEstimator):
         self._transform(X, return_expected_score=False)
         return self
 
-    def predict_proba(self, X: pd.DataFrame) -> np.ndarray:
-        preds = self._transform(X, return_expected_score=True)
-        return np.vstack((1 - preds, preds)).T  # type:ignore
 
-    def predict(self, X: pd.DataFrame) -> np.ndarray:
-        return self.predict_proba(X)[:, 1] > 0.5
+class EloEstimator(ClassifierRatingSystemMixin, BaseEloEstimator):
+    """
+    Elo rating system classifier.
+
+    Attributes
+    ----------
+    beta : float
+        Normalization factor for ratings when computing expected score.
+    columns : List[str]
+        [entity_1, entity_2, result] columns names.
+    default_init_rating : float
+        Default initial rating for entities.
+    entity_cols : Tuple[str, str]
+        Names of columns identifying the names of the entities playing the games.
+    init_ratings : Optional[Dict[str, Tuple[Optional[int], float]]]
+        Initial ratings for entities (dictionary of form entity: (Unix timestamp, rating))
+    k_factor : float
+        Elo K-factor/step-size for gradient descent.
+    k_factor_vec : Tuple[float, ...]
+        Vector of Elo K-factor/step-size for gradient descent.
+    model : Model
+        Underlying statistical model.
+    optimizer : Optimizer
+        Optimizer to update the model.
+    rating_history : List[Tuple[Optional[int], float]]
+        Historical ratings of entities (if track_rating_history is True).
+    score_col : str
+        Name of score column (1 if entity_1 wins and 0 if entity_2 wins).
+        Draws are not currently supported.
+    additional_regressors : Optional[List[Regressor]]
+        Additional regressors to include, e.g. home advantage.
+    track_rating_history : bool
+        Flag to track historical ratings of entities.
+
+    Methods
+    -------
+    fit(X, y=None)
+        Fit Elo rating system/calculate ratings.
+    record_ratings()
+        Record the current ratings of entities.
+    predict_proba(X)
+        Produce probability estimates.
+    predict(X)
+        Predict outcome of game.
+    """
+
+    def __init__(
+        self,
+        k_factor: float = 20,
+        default_init_rating: float = 1200,
+        beta: float = 200,
+        init_ratings: Optional[Dict[str, Tuple[Optional[int], float]]] = None,
+        entity_cols: Tuple[str, str] = ("entity_1", "entity_2"),
+        score_col: str = "score",
+        additional_regressors: Optional[List[Regressor]] = None,
+        track_rating_history: bool = False,
+    ) -> None:
+        """
+        Parameters
+        ----------
+        k_factor : float
+            Elo K-factor/step-size for gradient descent for the entities.
+        default_init_rating : float
+            Default initial rating for entities.
+        beta : float
+            Normalization factor for ratings when computing expected score.
+        init_ratings : Optional[Dict[str, Tuple[Optional[int], float]]]
+            Initial ratings for entities (dictionary of form entity: (Unix timestamp, rating))
+        entity_cols : Tuple[str, str]
+            Names of columns identifying the names of the entities playing the games.
+        score_col : str
+            Name of score column (1 if entity_1 wins and 0 if entity_2 wins).
+            Draws are not currently supported.
+        additional_regressors : Optional[List[Regressor]]
+            Additional regressors to include, e.g. home advantage.
+        track_rating_history : bool
+            Flag to track historical ratings of entities.
+        """
+        super().__init__(
+            model_type=LogisticRegression,
+            k_factor=k_factor,
+            default_init_rating=default_init_rating,
+            beta=beta,
+            init_ratings=init_ratings,
+            entity_cols=entity_cols,
+            score_col=score_col,
+            additional_regressors=additional_regressors,
+            track_rating_history=track_rating_history,
+        )
+
+
+class PoissonEloEstimator(RegressionRatingSystemMixin, BaseEloEstimator):
+    """
+    Poisson Elo rating system classifier.
+
+    Attributes
+    ----------
+    beta : float
+        Normalization factor for ratings when computing expected score.
+    columns : List[str]
+        [entity_1, entity_2, result] columns names.
+    default_init_rating : float
+        Default initial rating for entities.
+    entity_cols : Tuple[str, str]
+        Names of columns identifying the names of the entities playing the games.
+    init_ratings : Optional[Dict[str, Tuple[Optional[int], float]]]
+        Initial ratings for entities (dictionary of form entity: (Unix timestamp, rating))
+    k_factor : float
+        Elo K-factor/step-size for gradient descent.
+    k_factor_vec : Tuple[float, ...]
+        Vector of Elo K-factor/step-size for gradient descent.
+    model : Model
+        Underlying statistical model.
+    optimizer : Optimizer
+        Optimizer to update the model.
+    rating_history : List[Tuple[Optional[int], float]]
+        Historical ratings of entities (if track_rating_history is True).
+    score_col : str
+        Name of score column (1 if entity_1 wins and 0 if entity_2 wins).
+        Draws are not currently supported.
+    additional_regressors : Optional[List[Regressor]]
+        Additional regressors to include, e.g. home advantage.
+    track_rating_history : bool
+        Flag to track historical ratings of entities.
+
+    Methods
+    -------
+    fit(X, y=None)
+        Fit Elo rating system/calculate ratings.
+    record_ratings()
+        Record the current ratings of entities.
+    predict(X)
+        Predict score.
+    """
+
+    def __init__(
+        self,
+        k_factor: float = 20,
+        default_init_rating: float = 1200,
+        beta: float = 200,
+        init_ratings: Optional[Dict[str, Tuple[Optional[int], float]]] = None,
+        entity_cols: Tuple[str, str] = ("entity_1", "entity_2"),
+        score_col: str = "score",
+        additional_regressors: Optional[List[Regressor]] = None,
+        track_rating_history: bool = False,
+    ) -> None:
+        """
+        Parameters
+        ----------
+        k_factor : float
+            Elo K-factor/step-size for gradient descent for the entities.
+        default_init_rating : float
+            Default initial rating for entities.
+        beta : float
+            Normalization factor for ratings when computing expected score.
+        init_ratings : Optional[Dict[str, Tuple[Optional[int], float]]]
+            Initial ratings for entities (dictionary of form entity: (Unix timestamp, rating))
+        entity_cols : Tuple[str, str]
+            Names of columns identifying the names of the entities playing the games.
+        score_col : str
+            Name of score column (1 if entity_1 wins and 0 if entity_2 wins).
+            Draws are not currently supported.
+        additional_regressors : Optional[List[Regressor]]
+            Additional regressors to include, e.g. home advantage.
+        track_rating_history : bool
+            Flag to track historical ratings of entities.
+        """
+        super().__init__(
+            model_type=PoissonRegression,
+            k_factor=k_factor,
+            default_init_rating=default_init_rating,
+            beta=beta,
+            init_ratings=init_ratings,
+            entity_cols=entity_cols,
+            score_col=score_col,
+            additional_regressors=additional_regressors,
+            track_rating_history=track_rating_history,
+        )
