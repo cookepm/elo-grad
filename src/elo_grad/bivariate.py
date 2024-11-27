fc79094fa232a80b8b540fef8493d8a20fb40ade
Michael Cooke <cookepm@tcd.ie>
2024-11-27 07:47:54 +0000

    feat: Add BivariateOptimizer (#52)
    Abstract optimizer for bivariate models.

--- src/elo_grad/bivariate.py
+++ src/elo_grad/bivariate.py
@@ -1,8 +1,8 @@
 import abc
 
-from typing import Tuple
+from typing import Tuple, Optional, Generator
 
-from . import BaseModel
+from . import BaseModel, Regressor
 
 
 class BivariateModel(BaseModel, abc.ABC):
@@ -13,4 +13,24 @@ class BivariateModel(BaseModel, abc.ABC):
 
     @abc.abstractmethod
     def calculate_expected_scores(self, *args) -> Tuple[float, float]:
+        ...
+
+
+class BivariateOptimizer(abc.ABC):
+
+    @classmethod
+    @abc.abstractmethod
+    def _get_penalty(cls, model: BivariateModel, regressor: Regressor) -> float:
+        ...
+
+    @abc.abstractmethod
+    def calculate_update_step(
+        self,
+        model: BivariateModel,
+        y: Tuple[int, int],
+        entity_1: str,
+        entity_2: str,
+        additional_regressor_values: Optional[Tuple[float, ...]],
+        params: Optional[Tuple[float, ...]],
+    ) -> Generator[Tuple[float, ...], None, None]:
         ...
